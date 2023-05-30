fb7f0dabc239ef566c2421ed08ee98d4c861d7df
SSOOYA <bsy24660@naver.com>
2023-05-30 22:48:29 +0900

    [feat] #1: BaseCollectionView 템플릿 구현

--- HeyTicket/Presentation/Utils/Base/BaseCollectionViewCell.swift
+++ HeyTicket/Presentation/Utils/Base/BaseCollectionViewCell.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell{
+    
+    var cellIndexPath: IndexPath?{
+        (superview as? UICollectionView)?.indexPath(for: self)
+    }
+    
+    let baseView = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        template()
+        style()
+        hierarchy()
+        layout()
+        initialize()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    final private func template(){
+        setBaseView()
+        selectedBackgroundView = UIView()
+    }
+    
+    private func setBaseView(){
+        baseView.backgroundColor = .white
+        contentView.addSubview(baseView)
+        baseView.snp.makeConstraints{
+            $0.top.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    func style() { }
+    func hierarchy() { }
+    func layout() { }
+    func initialize() { }
+}
