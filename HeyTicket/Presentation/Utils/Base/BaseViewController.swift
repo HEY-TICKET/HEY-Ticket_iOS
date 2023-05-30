3b12107c3819ffa51e069d56e1f8d1029fd1920d
SSOOYA <bsy24660@naver.com>
2023-05-30 22:46:30 +0900

    [chore] #1: BaseViewController 템플릿 설정

--- HeyTicket/Presentation/Utils/Base/BaseViewController.swift
+++ HeyTicket/Presentation/Utils/Base/BaseViewController.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+import UIKit
+import RxSwift
+
+class BaseViewController<T: BaseView, P: BaseView>: UIViewController{
+    
+    let headerView: T
+    let mainView: P
+    
+    init(headerView: T, mainView: P) {
+        self.headerView = headerView
+        self.mainView = mainView
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    final let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        template()
+        style()
+        layout()
+        initialize()
+        bind()
+    }
+    
+    final private func template(){
+        setViewControllerStyle()
+    }
+    
+    private func setViewControllerStyle(){
+        view.backgroundColor = .white
+        navigationController?.navigationBar.isHidden = true
+//        navigationController?.interactivePopGestureRecognizer?.delegate = self
+        tabBarController?.tabBar.isHidden = true
+    }
+    
+    func style() { }
+    func layout() { }
+    func initialize() { }
+    func bind() { }
+}
