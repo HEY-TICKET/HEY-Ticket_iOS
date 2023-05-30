d7d85e1d329cd45ce9a669cae2cdc87ea8d4da91
SSOOYA <bsy24660@naver.com>
2023-05-30 15:27:46 +0900

    [chore] #1: 프로젝트 파일 등록

---
+++ HeyTicket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeyTicket
+//
+//  Created by 박소윤 on 2023/05/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
