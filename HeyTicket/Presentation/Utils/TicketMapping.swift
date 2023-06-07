39d004f3e884d545803cf886f5967d15ce929b25
SSOOYA <bsy24660@naver.com>
2023-06-07 16:09:04 +0900

    [feat] #7: TicketMapping 접근 제어, 데이터 반환 메서드 등 리팩토링

--- HeyTicket/Presentation/Utils/TicketMapping.swift
+++ HeyTicket/Presentation/Utils/TicketMapping.swift
@@ -9,22 +9,26 @@ import Foundation
 
 struct TicketMapping{
     
-    static let genre: [TicketGenre:String] = [
-        .THEATER = "연극",
-        .MUSICAL = "뮤지컬",
-        .CLASSIC = "클래식",
-        .KOREAN_TRADITIONAL_MUSIC = "국악",
-        .POPULAR_MUSIC = "대중음악",
-        .DANCE = "무용(서양/한국무용)",
-        .POPULAR_DANCE = "대중무용",
-        .CIRCUS_AND_MAGIC = "서커스/마술",
-        .MIXED_GENRE = "아동",
-        .ALL = "전체"
+    static func genre(index: Int) -> String {
+        let genre = TicketGenre.allCases[index]
+        return TicketMapping.genreMapping[genre]!
+    }
+    
+    static private let genreMapping: [TicketGenre : String] = [
+        .ALL : "전체",
+        .THEATER : "연극",
+        .MUSICAL : "뮤지컬",
+        .CLASSIC : "클래식",
+        .KOREAN_TRADITIONAL_MUSIC : "국악",
+        .POPULAR_MUSIC : "대중음악",
+        .DANCE : "무용(서양/한국무용)",
+        .POPULAR_DANCE : "대중무용",
+        .CIRCUS_AND_MAGIC : "서커스/마술",
+        .MIXED_GENRE : "아동",
     ]
     
     static let state: [TicketState:String] = [
-        .ONGOING = "공연 중",
-        .COMPLETED = "종료"
+        .ONGOING : "공연 중",
+        .COMPLETED : "종료"
     ]
-    
 }
