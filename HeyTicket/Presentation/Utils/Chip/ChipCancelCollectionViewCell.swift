ef99d7a8ceafc9675a86a6fedee38dac8db34f89
SSOOYA <bsy24660@naver.com>
2023-07-05 12:34:33 +0900

    [feat] : ChipCollectionViewCell 구현(기존 CategoryCollectionViewCell 대체 예정)

---
+++ HeyTicket/Presentation/Utils/Chip/ChipCancelCollectionViewCell.swift
@@ -0,0 +1,96 @@
+//
+//  ChipCancelCollectionViewCell.swift
+//  HeyTicket
+//
+//  Created by 박소윤 on 2023/06/29.
+//
+
+import HeyTicketKit
+import UIKit
+
+class ChipCancelCollectionViewCell: ChipCollectionViewCell{
+    
+    private let cancelImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = Image.cancel
+        return imageView
+    }()
+    
+    private static let selectAttribute: ChipAttribute = CancelChipSelectedAttribute()
+    private static let deselectAttribute: ChipAttribute = CancelChipDeselectedAttribute()
+    
+    override func style() {
+        selectedAttribute = ChipCancelCollectionViewCell.selectAttribute
+        deselectedAttribute = ChipCancelCollectionViewCell.deselectAttribute
+    }
+    
+    override func hierarchy() {
+        super.hierarchy()
+        titleBtn.addSubview(cancelImage)
+    }
+    
+    override func layout() {
+        super.layout()
+        cancelImage.snp.makeConstraints{
+            $0.width.height.equalTo(14)
+            $0.leading.equalTo(titleBtn.titleLabel!.snp.trailing).offset(8)
+            $0.trailing.equalToSuperview().inset(12)
+            $0.centerY.equalToSuperview()
+        }
+    }
+    
+    static func getSize(title: String, height: CGFloat = 30) -> CGSize{
+        let width = getWidth(
+            title: title,
+            selectAttribute: ChipCancelCollectionViewCell.selectAttribute,
+            deselectAttribute: ChipCancelCollectionViewCell.deselectAttribute
+        )
+        return CGSize(width: width, height: height)
+    }
+}
+
+//MARK: CategoryTagAttribute implement
+private extension ChipAttribute{
+    static var cornerAttribute: UIButton.Configuration.CornerStyle{
+        .capsule
+    }
+    static var insets: NSDirectionalEdgeInsets{
+        NSDirectionalEdgeInsets(top: 10, leading: 12, bottom: 8, trailing: 34)
+    }
+}
+
+private struct CancelChipSelectedAttribute: ChipAttribute{
+    let configuration: UIButton.Configuration = {
+        var configuration = UIButton.Configuration.filled()
+        configuration.baseBackgroundColor = .black
+        configuration.cornerStyle = cornerAttribute
+        configuration.contentInsets = insets
+        return configuration
+    }()
+    
+    let titleAttributes: AttributeContainer = {
+        var container = AttributeContainer()
+        container.font = Typo.appleFont(type: .Medium, size: 14)
+        container.foregroundColor = .white
+        return container
+    }()
+}
+
+struct CancelChipDeselectedAttribute: ChipAttribute{
+    let configuration: UIButton.Configuration = {
+        var configuration = UIButton.Configuration.bordered()
+        configuration.background.strokeColor = Color.grey200
+        configuration.background.strokeWidth = 1
+        configuration.baseBackgroundColor = .white
+        configuration.cornerStyle = cornerAttribute
+        configuration.contentInsets = insets
+        return configuration
+    }()
+    
+    let titleAttributes: AttributeContainer = {
+        var container = AttributeContainer()
+        container.font = Typo.appleFont(type: .Regular, size: 14)
+        container.foregroundColor = Color.grey500
+        return container
+    }()
+}
